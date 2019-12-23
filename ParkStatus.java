64a073fae32f2d997feed5c97122740ebe5335be
oguztimurtasdelen <48294332+oguztimurtasdelen@users.noreply.github.com>
2019-12-23 06:34:08 +0300

    Update ParkStatus.java
    Abone ve Abone Olmayan park işlemleri için Superclass olarak kullanılacak.

--- ParkStatus.java
+++ ParkStatus.java
@@ -1,5 +1,90 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class ParkStatus 
+public class ParkStatus
 {
+	String carPlate;
+	private String fullName;
+	private String phoneNumber;
+	private String eMail;
+	String place;
+	String entryTime = getDateTime();
+	String getDateTime()
+	{
+		DateFormat dateTimeFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+		Date date = new Date();
+		return dateTimeFormat.format(date);
+		
+	}
+	
+	
+	
+	
+	
+	public String getCarPlate() 
+	{
+		return carPlate;
+	}
+	public void setCarPlate(String carPlate) 
+	{
+		this.carPlate = carPlate;
+	}
+	
+	
+	
+	public String getFullName() 
+	{
+		return fullName;
+	}
+	public void setFullName(String fullName) 
+	{
+		this.fullName = fullName;
+	}
+	
+	
+	
+	public String getPhoneNumber() 
+	{
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) 
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	
+	public String geteMail() 
+	{
+		return eMail;
+	}
+	public void seteMail(String eMail) 
+	{
+		this.eMail = eMail;
+	}
+	
+	
+	
+	public String getPlace() 
+	{
+		return place;
+	}
+	public void setPlace(String place) 
+	{
+		this.place = place;
+	}
+	
+	
+	
+	public String getEntryTime() 
+	{
+		return entryTime;
+	}
+	public void setEntryTime(String entryTime) 
+	{
+		this.entryTime = entryTime;
+	}
+	
 
 }
